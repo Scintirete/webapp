e17ea4b2445e19e00e51e409216b98d308140ee8
wang <admin@wj2015.com>
2025-08-06 08:16:06 +0800

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
-  },
-  output: 'standalone', // 为 Docker 优化输出
+  // turbopack: {
+  //   rules: {
+  //     '*.svg': {
+  //       loaders: ['@svgr/webpack'],
+  //       as: '*.js',
+  //     },
+  //   },
+  // },
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
